c7548c2c22ea61aa272c7f1f8a689358f5c50c03
GastonCarg <gastoncargnelutti@gmail.com>
2022-05-24 16:24:38 -0300

    correciones varias en el index.html y agregadas (todas?) las validaciones

--- colas.js
+++ colas.js
@@ -8,10 +8,7 @@ const tomarInputs = () => {
         return alert("X, N, DESDE, HASTA: los valores deben ser mayores a 0.");
     if (isNaN(x) || isNaN(n) || isNaN(desde) || isNaN(hasta))
         return alert("X/N/DESDE/HASTA: por favor, ingrese todos los datos.");
-    if (Number.isInteger(x) && Number.isInteger(n)){
-        //no hace nada
-    }
-    else{
+    if (!Number.isInteger(x) && !Number.isInteger(n)){
         return alert("X y N deben ser enteros");
     }
 
@@ -75,7 +72,65 @@ const tomarInputs = () => {
         trab_e,
     ]);
 
-    // TODO: validar los inputs faltantes
+    // Tiempos de los trabajos
+    const time_trab_a = parseInt(document.getElementById("time-trab-a").value);
+    const time_trab_b = parseInt(document.getElementById("time-trab-b").value);
+    const time_trab_c = parseInt(document.getElementById("time-trab-c").value);
+    const time_trab_d = parseInt(document.getElementById("time-trab-d").value);
+    const time_trab_e = parseInt(document.getElementById("time-trab-e").value);
+
+    if (
+        isNaN(time_trab_a) ||
+        isNaN(time_trab_b) ||
+        isNaN(time_trab_c) ||
+        isNaN(time_trab_d) ||
+        isNaN(time_trab_e)
+    )
+        return alert("Por favor, ingrese todos los datos.");
+    if (
+        time_trab_a < 0 ||
+        time_trab_b < 0 ||
+        time_trab_c < 0 ||
+        time_trab_d < 0 ||
+        time_trab_e < 0
+    )
+        return alert("Los tiempos deben ser mayores a 0.");
+
+    // Tiempo que se suma a los valores de los trabajos
+    const distrib_trab_a = parseInt(document.getElementById("distrib-trab-a").value);
+    const distrib_trab_b = parseInt(document.getElementById("distrib-trab-b").value);
+
+    if (
+        isNaN(distrib_trab_a) ||
+        isNaN(distrib_trab_b)
+    )
+        return alert("Por favor, ingrese todos los datos.");
+    if (
+        distrib_trab_a < 0 ||
+        distrib_trab_b < 0
+    )
+        return alert("Los valores de A y B deben ser mayores a 0.");
+
+    if (distrib_trab_a >= distrib_trab_b)
+        return alert("El valor de A debe ser menor al valor de B.");
+
+    // Minutos antes y despues del trabajo de formateo
+    const prim_min_trab_c = parseInt(document.getElementById("prim-min-trab-c").value);
+    const ult_min_trab_c = parseInt(document.getElementById("ult-min-trab-c").value);
+    if (
+        isNaN(prim_min_trab_c) ||
+        isNaN(ult_min_trab_c)
+    )
+        return alert("Por favor, ingrese todos los datos.");
+    if (
+        prim_min_trab_c < 0 ||
+        ult_min_trab_c < 0
+    )
+        return alert("Los valores de los primeros y ultimos min deben ser mayores a 0.");
+    if (
+        (prim_min_trab_c + ult_min_trab_c) >= time_trab_c
+    )
+        return alert("La suma de los primeros y ultimos minutos debe ser menor al tiempo del trabajo.");
 
     return [
         n,
@@ -83,6 +138,33 @@ const tomarInputs = () => {
         desde,
         hasta,
         prob_acum_trabajos,
+        time_trab_a,
+        time_trab_b,
+        time_trab_c,
+        time_trab_d,
+        time_trab_e,
 
     ];
 };
+
+const calcularProbabilidadAcumulada = (probs) => {
+    let acu = 0;
+    let probs_acum = [];
+
+    for (let i = 0; i < probs.length; i++) {
+        acu += probs[i];
+        probs_acum[i] = acu;
+    }
+
+    return probs_acum;
+};
+
+const armarIntervalos = () => {
+    let lim_inf = 0;
+    let lim_sup = 0;
+    let lim_superiores = [];
+    let lim_inferiores = [];
+    let prob_acum = 0;
+
+    return intervalos;
+}
